b97fec85400921d403d8803e5dac67376e6b1a6d
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-10 11:29:21 -0700

    handle all single end read file types

--- scripts/agave_to_soapdenovo2_cmd_line_args.py
+++ scripts/agave_to_soapdenovo2_cmd_line_args.py
@@ -53,21 +53,30 @@ def get_args(argv):
 def extend_config_file_content(config_file_content, agave_cmd_line_args):
     # the options in this table need conversion from Agave to soapdenovo2
     input_option_table = {
+        '-b': list(),
+        '-f': list(),
+        '-p': list(),
+        '-q': list(),
         '-f1': list(),
         '-f2': list(),
         '-q1': list(),
         '-q2': list()
     }
 
-    additional_args = []
+    additional_args_list = []
 
+    # segregate args from unknown_arg_list into the
+    # input_option_table or the additional_args_list
     unknown_arg_list = list(agave_cmd_line_args)
     while len(unknown_arg_list) > 0:
         next_arg = unknown_arg_list.pop(0)
         if next_arg in input_option_table.keys():
             input_option_table[next_arg].append(unknown_arg_list.pop(0))
         else:
-            additional_args.append(next_arg)
+            additional_args_list.append(next_arg)
+
+    input_option_table['f12'] = list(zip(input_option_table['-f1'], input_option_table['-f2']))
+    input_option_table['q12'] = list(zip(input_option_table['-q1'], input_option_table['-q2']))
 
     # combine FASTA and FASTQ paired-end lists
     extended_config_file_buffer = io.StringIO()
@@ -79,10 +88,13 @@ def extend_config_file_content(config_file_content, agave_cmd_line_args):
       else:
         extended_config_file_buffer.write(stripped_line)
         extended_config_file_buffer.write('\n')
-    input_option_table['f'] = list(zip(input_option_table['-f1'], input_option_table['-f2']))
-    input_option_table['q'] = list(zip(input_option_table['-q1'], input_option_table['-q2']))
 
-    for opt, arg_list in [(opt_, input_option_table[opt_]) for opt_ in ('f', 'q')]:
+    for (dash, opt), arg_list in [(opt_, input_option_table[opt_]) for opt_ in ('-f', '-q', '-b', '-p')]:
+        while len(arg_list) > 0:
+            fp = arg_list.pop(0)
+            extended_config_file_buffer.write('{}={}\n'.format(opt, fp))
+
+    for (opt, *_), arg_list in [(opt_, input_option_table[opt_]) for opt_ in ('f12', 'q12')]:
         while len(arg_list) > 0:
             fp1, fp2 = arg_list.pop(0)
             extended_config_file_buffer.write('{}1={}\n'.format(opt, fp1))
@@ -118,19 +130,84 @@ def test_agave_to_soapdenovo2_cmd_line_args():
     assert cmd_line_args == ['-f1', 'file1.fa', '-f2', 'file2.fa']
 
 
+def test_single_end_fasta_conversions():
+    # throw in some extra newlines to see that they are removed
+    extended_config_file_content = extend_config_file_content(
+        '[LIB]\n\n',
+        ['-f', 'file1.fa']
+    )
+
+    assert extended_config_file_content == """\
+[LIB]
+f=file1.fa
+"""
+
+    extended_config_file_content = extend_config_file_content(
+            '[LIB]\n\n',
+            ['-f', 'file1.fa', '-f', 'file2.fa']
+        )
+
+    assert extended_config_file_content == """\
+[LIB]
+f=file1.fa
+f=file2.fa
+"""
+
+
+def test_single_end_fastq_conversions():
+    # throw in some extra newlines to see that they are removed
+    extended_config_file_content = extend_config_file_content(
+        '[LIB]\n\n',
+        ['-q', 'file1.fq', '-q', 'file2.fq', '-q', 'file3.fq']
+    )
+
+    assert extended_config_file_content == """\
+[LIB]
+q=file1.fq
+q=file2.fq
+q=file3.fq
+"""
+
+
+def test_single_file_paired_end_fasta_conversions():
+    # throw in some extra newlines to see that they are removed
+    extended_config_file_content = extend_config_file_content(
+        '[LIB]\n\n',
+        ['-p', 'file1.fa', '-p', 'file2.fa']
+    )
+
+    assert extended_config_file_content == """\
+[LIB]
+p=file1.fa
+p=file2.fa
+"""
+
+
+def test_single_file_paired_end_bam_conversions():
+    # throw in some extra newlines to see that they are removed
+    extended_config_file_content = extend_config_file_content(
+        '[LIB]\n\n',
+        ['-b', 'file1.bam', '-b', 'file2.bam']
+    )
+
+    assert extended_config_file_content == """\
+[LIB]
+b=file1.bam
+b=file2.bam
+"""
+
+
 def test_paired_end_fasta_conversions():
     # throw in some extra newlines to see that they are removed
     extended_config_file_content = extend_config_file_content(
         '[LIB]\n\n',
-        ['-f1', 'file1.fa', '-f2', 'file2.fa', '-f1', 'file3.fa', '-f2', 'file4.fa']
+        ['-f1', 'file1.fa', '-f2', 'file2.fa']
     )
 
     assert extended_config_file_content == """\
 [LIB]
 f1=file1.fa
 f2=file2.fa
-f1=file3.fa
-f2=file4.fa
 """
 
 
