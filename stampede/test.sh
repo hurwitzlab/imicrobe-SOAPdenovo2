881f1ebdde55a8c176c2f5b7621aefd690c008f5
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-09 17:11:43 -0700

    get test data from /iplant/home/shared/iplant_training

--- stampede/test.sh
+++ stampede/test.sh
@@ -5,9 +5,26 @@
 #SBATCH -n 16
 #SBATCH -t 00:30:00
 #SBATCH -p development
-#SBATCH -J test-SOAPdenovo2
+#SBATCH -J test-imicrobe-soapdenovo2
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --mail-user jklynch@email.arizona.edu
 
-OUT_DIR="$SCRATCH/imicrobe-SOAPdenovo2/test"
+module load irods
+
+INPUT_DIR="$SCRATCH/imicrobe-soapdenovo2/test/input"
+if [[ -d $INPUT_DIR ]]; then
+  rm -rf $INPUT_DIR
+fi
+
+mkdir -p $INPUT_DIR
+iget /iplant/home/shared/iplant_training/genome_assembly_soapdenovo/A_Assemble_Reads/fragScSi_1.fq $INPUT_DIR
+iget /iplant/home/shared/iplant_training/genome_assembly_soapdenovo/A_Assemble_Reads/fragScSi_2.fq $INPUT_DIR
+iget /iplant/home/jklynch/test/imicrobe-soapdenovo2/test.config $INPUT_DIR
+INPUT_FILE_1=$INPUT_DIR/fragScSi_1.fq
+INPUT_FILE_2=$INPUT_DIR/fragScSi_2.fq
+$CONFIG_FILE=$INPUT_DIR/test.config
+
+OUT_DIR="$SCRATCH/imicrobe-soapdenovo2/test/output"
 
 if [[ -d $OUT_DIR ]]; then
   rm -rf $OUT_DIR
@@ -15,4 +32,4 @@ fi
 
 mkdir -p $OUT_DIR
 
-./run.sh "$SCRATCH/imicrobe-SOAPdenovo2/test-data/" $OUT_DIR
+./run.sh $CONFIG_FILE -o $OUT_DIR -q1 $INPUT_FILE_1 -q2 $INPUT_FILE_2
