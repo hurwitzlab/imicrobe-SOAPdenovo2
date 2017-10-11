ef3b56f83eee20c534fa5bfec7f3bd5cf94d42a7
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-11 15:56:41 -0700

    configuration file is now $2

--- stampede/run.sh
+++ stampede/run.sh
@@ -2,10 +2,12 @@
 
 module load singularity
 
+echo "$@"
+
 SOAPDENOVO2_CMD_LINE_ARGS=`singularity exec imicrobe-soapdenovo2.img python3 /scripts/agave_to_soapdenovo2_cmd_line_args.py $@`
 
 echo "SOAPdenovo2 command line args: \"${SOAPDENOVO2_CMD_LINE_ARGS}\""
 echo "SOAPdenovo2 configuration file:"
-cat $1
+cat $2
 
 singularity run imicrobe-soapdenovo2.img ${SOAPDENOVO2_CMD_LINE_ARGS}
